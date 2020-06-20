d9236b13d29816b59e457a2b26c6ad7c284aeb01
LuizFhelipy <fhelipy_rt@hotmail.com>
2020-06-20 15:15:14 -0300

    alterações nas imagens

--- javascript/sprite.js
+++ javascript/sprite.js
@@ -19,11 +19,12 @@ inicio = new Sprite(0, 0, 200, 205, "img/start.png")
 spriteGameover = new Sprite(0, 0, 256, 256, "img/gameover.png")
 bg = new Sprite(0, 0, window.innerWidth, window.innerHeight, "img/konoha.png")
 spritePedras = new Sprite(10, 1, 145, 107, "img/orochimaru.png")
+tutorial = new Sprite(0,0,500,300, "img/tutorial.png")
 
 runBoneco = [
-    new Sprite(44, 30, 145, 185,  "img/boneco.png"),
-    new Sprite(190, 47, 145, 185, "img/boneco.png"),
-    new Sprite(371, 55, 145, 185, "img/boneco.png"),
-    new Sprite(568, 44, 145, 185, "img/boneco.png"),
-    new Sprite(736, 55, 145, 185, "img/boneco.png")
+    new Sprite(44, 30, 145, 185,  "img/naruto.png"),
+    new Sprite(190, 47, 145, 185, "img/naruto.png"),
+    new Sprite(371, 55, 145, 185, "img/naruto.png"),
+    new Sprite(568, 44, 145, 185, "img/naruto.png"),
+    new Sprite(736, 55, 145, 185, "img/naruto.png")
 ]
